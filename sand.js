7933c7f75e664d45d6015c8ed1266cf0684aac5b
ToastedThomas <96932052+ToastedThomas@users.noreply.github.com>
2022-11-12 17:35:11 -0600

    Fixed current issues with new features
    User interface buttons now glow on hover, and a description of the element is displayed below the element list. Useful information such as current element and mouse location is now displayed at the top of the screen.

--- sand.js
+++ sand.js
@@ -6,25 +6,65 @@ var cvs = document.getElementById('cvs');
 cvs.width = width * cellSize;
 cvs.height = height * cellSize;
 var ctx = cvs.getContext('2d');
+//info for canvas above
+
 var buffer = [];
-var EMPTY = 0;
-var WALL = 1;
-var SAND = 2;
-var WATER = 3;
-var OIL = 4;
-var FIRE = 5;
-var colors = {};
-colors[EMPTY] = '#999';
-colors[WALL] = '#444';
-colors[SAND] = '#ff4';
-colors[WATER] = '#00f';
-colors[OIL] = '#324D25';
-colors[FIRE] = '#C52A29';
-var selectedItem = SAND;
+
+let EMPTY = {
+    id: 0,
+    color: '#999999',
+    isSolid: false,
+    btn: document.getElementById('void'),
+    desc: 'Empty, nothing, eraser.',
+    name: 'Void'
+}
+let WALL = {
+    id: 1,
+    color: '#444444',
+    isSolid: true,
+    btn: document.getElementById('wall'),
+    desc: 'Walls prevent things from moving. Walls are not effected by gravity.',
+    name: 'Wall'
+}
+let SAND = {
+    id: 2,
+    color: '#ffff44',
+    isSolid: true,
+    btn: document.getElementById('sand'),
+    desc: 'Sand particles scatter when falling.',
+    name: 'Sand'
+}
+let WATER = {
+    id: 3,
+    color: '#0000ff',
+    isSolid: false,
+    btn: document.getElementById('water'),
+    desc: 'Water does not mix with oil. Water can be evaporated with fire.',
+    name: 'Water'
+}
+let OIL = {
+    id: 4,
+    color: '#324D25',
+    isSolid: false,
+    btn: document.getElementById('oil'),
+    desc: 'Oil does not mix with water. Oil can also be ignited.',
+    name: 'Oil'
+}
+let FIRE = {
+    id: 5,
+    color: '#C52A29',
+    isSolid: false,
+    btn: document.getElementById('fire'),
+    desc: 'Fire can ignite oil. Fire also evaporates water.',
+    name: 'Fire'
+}
+let Elements = [EMPTY, WALL, SAND, WATER, OIL, FIRE];
+
+var selectedItem = SAND.id;
 
 // set all cells to empty
 for (var i = 0; i < width * height; i++)
-    buffer[i] = EMPTY;
+    buffer[i] = EMPTY.id;
 
 // set buffer at location (x, y)
 function setBuf(x, y, val) {
@@ -35,76 +75,73 @@ function setBuf(x, y, val) {
 function getBuf(x, y) {
     if (x < 0 || x >= width ||
         y < 0 || y >= height)
-        return EMPTY;
-    return buffer[x + y * width];
+        return EMPTY.id;
+    return buffer[x + y * width]
 }
 
 // put some wall (1) down
 for (var x = -10; x <= 10; x++)
-    setBuf(Math.floor(width / 2) + x, height - 10, WALL);
+    setBuf(Math.floor(width / 2) + x, height - 10, WALL.id);
 
 // put edges on wall
 for (var y = height - 10; y > height - 20; y--) {
-    setBuf(Math.floor(width / 2) - 10, y, WALL);
-    setBuf(Math.floor(width / 2) + 10, y, WALL);
+    setBuf(Math.floor(width / 2) - 10, y, WALL.id);
+    setBuf(Math.floor(width / 2) + 10, y, WALL.id);
 }
 
-document.getElementById('wall').onclick = function() {selectItem(WALL);}
-document.getElementById('sand').onclick = function() {selectItem(SAND);}
-document.getElementById('water').onclick = function() {selectItem(WATER);}
-document.getElementById('void').onclick = function() {selectItem(EMPTY);}
-document.getElementById('fire').onclick = function() {selectItem(FIRE);}
-document.getElementById('oil').onclick = function() {selectItem(OIL);}
+Elements.forEach(element => element.btn.onclick = function() {selectItem(element.id);});//connects ui buttons to select item function
+Elements.forEach(element => element.btn.style.backgroundColor = element.color);//sets the color of the elements ui buttons
+Elements.forEach(element => element.btn.onmouseenter = function() {changeDescription(element.desc);});//links button hover events to function with element desc
+Elements.forEach(element => element.btn.onmouseleave = function() {clearDescription();});//clears the description text
 
-// setting color of buttons with stored color
-document.getElementById('wall').style.backgroundColor = colors[WALL];
-document.getElementById('sand').style.backgroundColor = colors[SAND];
-document.getElementById('water').style.backgroundColor = colors[WATER];
-document.getElementById('void').style.backgroundColor = colors[EMPTY];
-document.getElementById('oil').style.backgroundColor = colors[OIL];
-document.getElementById('fire').style.backgroundColor = colors[FIRE];
 
 function selectItem(item) {
   //make clicked on div the selected element
   selectedItem = item;
-  console.log(selectedItem)
+  document.getElementById('currentElement').innerHTML = Elements[item].name;
 }
 
-var emptyOrLiquid = [EMPTY, WATER, OIL];
+function changeDescription(desc) {
+    document.getElementById('descText').innerHTML = desc;
+}
+function clearDescription() {
+    document.getElementById('descText').innerHTML = '';
+}
 
 function think() {
     for (var y = height - 1; y >= 0; y--) {
         var moveHoriz = [];
         for (var x = 0; x < width; x++) {
             // set dir to +1 or -1 randomly
-            var dir = Math.random() < 0.5 ? -1 : 1;
-            if (getBuf(x, y) == SAND) { // if we have sand
-                if (emptyOrLiquid.indexOf(getBuf(x, y + 1)) >= 0) { // if empty/liquid below
-                    if (emptyOrLiquid.indexOf(getBuf(x, y + 2)) >= 0) { //if more space to fall
-                        if (Math.random() < 0.1 && emptyOrLiquid.indexOf(getBuf(x + dir, y + 1)) >= 0) { //if shifted position is not a wall
-                            setBuf(x, y, getBuf(x + dir, y + 1)); // clear sand
-                            setBuf(x + dir, y + 1, SAND); // move sand shifted over
+            let randomNum = Math.random()
+            var dir = randomNum < 0.5 ? -1 : 1;
+            if (getBuf(x, y) == SAND.id) { // if we have sand
+                if (!Elements[getBuf(x, y + 1)].isSolid) { // if empty/liquid below
+                    if (!Elements[getBuf(x, y + 2)].isSolid) { //if more space to fall
+                        if (Math.random() < 0.1 && Elements[getBuf(x + dir, y + 1)].isSolid == false) { //if shifted position is not a wall
+                            setBuf(x, y, Elements[getBuf(x + dir, y + 1)].id); // clear sand
+                            setBuf(x + dir, y + 1, SAND.id); // move sand shifted over
                         } else {
-                            setBuf(x, y, getBuf(x, y + 1)); // clear sand
-                            setBuf(x, y + 1, SAND); // move sand
+                            setBuf(x, y, Elements[getBuf(x, y + 1)].id); // clear sand
+                            setBuf(x, y + 1, SAND.id); // move sand
                         }
                     } else {
-                        setBuf(x, y, getBuf(x, y + 1)); // clear sand
-                        setBuf(x, y + 1, SAND); // move sand
+                        setBuf(x, y, Elements[getBuf(x, y + 1)].id); // clear sand
+                        setBuf(x, y + 1, SAND.id); // move sand
                     }
-                } else if (emptyOrLiquid.indexOf(getBuf(x + dir, y + 1)) >= 0) { // if empty/liquid diagonal
-                   if (emptyOrLiquid.indexOf(getBuf(x + dir, y)) >= 0) { // checks for wall in direction of travel, prevents falling through "cracks"
-                        setBuf(x, y, getBuf(x + dir, y + 1)); // clear sand
-                        setBuf(x + dir, y + 1, SAND); // move sand
+                } else if (!Elements[getBuf(x + dir, y + 1)].isSolid) { // if empty/liquid diagonal
+                   if (!Elements[getBuf(x + dir, y)].isSolid) { // checks for wall in direction of travel, prevents falling through "cracks"
+                        setBuf(x, y, Elements[getBuf(x + dir, y + 1)].id); // clear sand
+                        setBuf(x + dir, y + 1, SAND.id); // move sand
                     }
                 } 
-            } else if (getBuf(x, y) == WATER) { // if we have water
-                if (getBuf(x, y + 1) == EMPTY || getBuf(x, y + 1) == OIL) { // if empty below
-                    setBuf(x, y, getBuf(x, y + 1)); // clear water
-                    setBuf(x, y + 1, WATER); // move water
-                } else if (getBuf(x + dir, y) == EMPTY || getBuf(x + dir, y) == OIL) {
-                    setBuf(x, y, getBuf(x + dir, y)); // clear water
-                    setBuf(x + dir, y, WATER); // move water
+            } else if (getBuf(x, y) == WATER.id) { // if we have water
+                if (getBuf(x, y + 1) == EMPTY.id || getBuf(x, y + 1) == OIL.id) { // if empty below
+                    setBuf(x, y, Elements[getBuf(x, y + 1)].id); // clear water
+                    setBuf(x, y + 1, WATER.id); // move water
+                } else if (getBuf(x + dir, y) == EMPTY.id || getBuf(x + dir, y) == OIL.id) {
+                    setBuf(x, y, Elements[getBuf(x + dir, y)].id); // clear water
+                    setBuf(x + dir, y, WATER.id); // move water
                     /*moveHoriz.push({
                             x: x,
                             y: y,
@@ -112,39 +149,38 @@ function think() {
                             element: WATER
                         });*/
                 }
-            } else if (getBuf(x, y) == OIL) { // if we have oil
-                if (getBuf(x, y + 1) == EMPTY) { // if empty below
-                    setBuf(x, y, EMPTY); // clear oil
-                    setBuf(x, y + 1, OIL); // move oil
-                } else if (getBuf(x + dir, y) == EMPTY || getBuf(x + dir, y) == WATER) {
+            } else if (getBuf(x, y) == OIL.id) { // if we have oil
+                if (getBuf(x, y + 1) == EMPTY.id) { // if empty below
+                    setBuf(x, y, Elements[getBuf(x, y + 1)].id); // clear oil
+                    setBuf(x, y + 1, OIL.id); // move oil
+                } else if (getBuf(x + dir, y) == EMPTY.id || getBuf(x + dir, y) == WATER.id) {
                     moveHoriz.push({
                             x: x,
                             y: y,
                             nx: x + dir,
-                            element: OIL
+                            element: OIL.id
                         });
                 }
-            } else if (getBuf(x, y) == FIRE) { // if we have fire
-                if (getBuf(x, y - 1) == OIL) { // if oil is above
-                    setBuf(x, y - 1, FIRE); // turn oil to fire
+            } else if (getBuf(x, y) == FIRE.id && randomNum < 0.25) { // if we have fire
+                if (getBuf(x, y - 1) == OIL.id) { // if oil is above
+                    setBuf(x, y - 1, FIRE.id); // turn oil to fire
                 }
-                if (getBuf(x, y + 1) == OIL) {
-                    setBuf(x, y + 1, FIRE);
+                if (getBuf(x, y + 1) == OIL.id) {
+                    setBuf(x, y + 1, FIRE.id);
                 }
-                if (getBuf(x - 1, y) == OIL) { 
-                    setBuf(x - 1, y, FIRE);
+                if (getBuf(x - 1, y) == OIL.id) { 
+                    setBuf(x - 1, y, FIRE.id);
                 }
-                if (getBuf(x + 1, y) == OIL) {
-                    setBuf(x + 1, y, FIRE);
+                if (getBuf(x + 1, y) == OIL.id) {
+                    setBuf(x + 1, y, FIRE.id);
                 }
-                setTimeout(fireBurnOut(x, y), 1000);
+                setTimeout(fireBurnOut(x, y), (700 + (randomNum * 1000)));
             }
         }
         for (var i = 0; i < moveHoriz.length; i++) {
             var m = moveHoriz[i];
-            if (getBuf(m.x, m.y) == m.element &&
-                getBuf(m.nx, m.y) == EMPTY) {
-                setBuf(m.x, m.y, EMPTY); // clear element
+            if (getBuf(m.x, m.y) == m.element && getBuf(m.nx, m.y) == EMPTY.id) {
+                setBuf(m.x, m.y, EMPTY.id); // clear element
                 setBuf(m.nx, m.y, m.element); // move element
             }
         }
@@ -152,13 +188,15 @@ function think() {
 }
 
 function fireBurnOut(x, y) { // turns fire back into empty
-    setBuf(x, y, EMPTY);
+    setBuf(x, y, EMPTY.id);
 }
 
 function draw() {
     for (var y = 0; y < height; y++) {
         for (var x = 0; x < width; x++) {
-            ctx.fillStyle = colors[getBuf(x, y)];
+            let location = getBuf(x, y);
+            
+            ctx.fillStyle = Elements[location].color;
             ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
         }
     }
@@ -167,51 +205,44 @@ function draw() {
 let placingInterval;
 let cursorX;
 let cursorY;
-let marginVal = parseInt(getComputedStyle(cvs).getPropertyValue('margin-left'));
-document.getElementById('elementHolder').style.marginLeft = marginVal + 'px';
-console.log(marginVal/10)
 
-cvs.onmousemove = function(event) {
-  //tracking mouse location for placing
-  cursorX = event.clientX;
-  cursorY = event.clientY;
+cvs.onmousemove = function(event) {//tracking mouse location for placing
+  cursorX = event.clientX - 180;
+  cursorY = event.clientY - 60;
+  document.getElementById('xCord').innerHTML = Math.floor(cursorX/10);
+  document.getElementById('yCord').innerHTML = Math.floor(cursorY/10);
 }
-cvs.onmousedown = function(event) {
-  //setting the rate at how fast elements are placed
+cvs.onmousedown = function(event) {//setting the rate at how fast elements are placed
   placingInterval = setInterval(placing, 20, event);
 }
-cvs.touchmove = function(event) {
-    //tracking finger location for mobile placing
+cvs.touchmove = function(event) {//tracking finger location for mobile placing
     cursorX = event.clientX;
     cursorY = event.clientY;
 }
-cvs.touchstart = function(event) {
-    //setting the rate at how fast elements are placed
+cvs.touchstart = function(event) {//setting the rate at how fast elements are placed
     placingInterval = setInterval(placing, 20, event);
 }
+
 function placing(event) {
   marginVal = parseInt(getComputedStyle(cvs).getPropertyValue('margin-left'));
-  setBuf((Math.floor(cursorX/10) - 1 - Math.floor(marginVal/10)) /* + (Math.floor(Math.random() * 3) - 1)*/, Math.floor(cursorY/10) - 1, selectedItem);
-  //console.log(Math.floor(cursorX/10) - 1)
-  //console.log(Math.floor(cursorY/10) - 1)
+  setBuf((Math.floor(cursorX/10)) /* + (Math.floor(Math.random() * 3) - 1)*/, Math.floor(cursorY/10), selectedItem);
+  console.log(Math.floor(cursorX/10))
+  console.log(Math.floor(cursorY/10))
 }
-cvs.onmouseup = function() {
-  //turns off the placing loop
+
+cvs.onmouseup = function() {//turns off the placing loop
   clearInterval(placingInterval);
   placingInterval = null;
 }
-cvs.onmouseleave = function() {
-  //also turns off the placing loop
+cvs.onmouseleave = function() {//also turns off the placing loop
   clearInterval(placingInterval);
   placingInterval = null;
 }
-cvs.touchend = function() {
-    //turns off the placing loop for mobile
+cvs.touchend = function() {//turns off the placing loop for mobile
     clearInterval(placingInterval);
     placingInterval = null;
 }
-cvs.touchcancel = function() {
-    //also turns off the placing loop for mobile
+cvs.touchcancel = function() {//also turns off the placing loop for mobile
     clearInterval(placingInterval);
     placingInterval = null;
   }
